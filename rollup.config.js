38f4281f9be86adbbbd1f9a7f7e36d5b7a6ef9c8
Nir Yariv <nyariv@gmail.com>
2020-09-04 17:08:26 +0300

    added css file

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,12 @@ import babel from '@rollup/plugin-babel';
 import filesize from 'rollup-plugin-filesize';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import copy from "rollup-plugin-copy-assets";
 
 const extensions = ['.js', '.ts']
 
 export default {
-  input: 'src/Scope.ts',
+  input: ['src/Scope.ts', 'src/defaultInit.ts'],
   output: {
     dir: 'dist',
     name: 'ScopeJS',
@@ -15,6 +16,11 @@ export default {
     sourcemap: true
   },
   plugins: [
+    copy({
+      assets: [
+        "src/scopejs.css",
+      ],
+    }),
     typescript(),
     resolve({ extensions }),
     babel({ extensions, babelHelpers: 'bundled' }),
